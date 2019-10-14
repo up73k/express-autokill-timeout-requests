3e386a163090433e856a913d2c5a2e640645922b
up73k <uptech@ya.ru>
2019-10-14 14:03:16 +0300

    Use Promise.race

--- index.js
+++ index.js
@@ -9,14 +9,23 @@ const sleep = function (timeout) {
 app.get('/', asyncHandler(async function (req, res) {
   const startTime = new Date();
   try {
-    await new Promise((resolve, reject) => {
+    let resolver;
+    const promise = new Promise((resolve, reject) => {
       setTimeout(() => reject(new Error(`Time elapsed ${(new Date() - startTime)/1000}s`)), 5000);
+      resolver = resolve;
+    })
+    .then(() => {
+      console.log('Timeout resolved');
+    })
+    .catch(() => {
+      console.log('Timeout rejected');
     });
 
-    await sleep(10000);
+    await Promise.race([sleep(10000), promise]);
 
     res.send('Hello World');
     console.log(`Request complete ${(new Date() - startTime)/1000}s`);
+    resolver();
   } catch (err) {
     console.log(err.message);
     res.send(err.message);
